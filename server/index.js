7b8d25bb82340d0112a8cc058c28ad1ef48974ed
Bishwajit Jha <ajitjha393@gmail.com>
2021-04-08 09:23:39 +0530

    Added endpoints and static folder

--- server/index.js
+++ server/index.js
@@ -9,6 +9,11 @@ app.use(helmet());
 app.use(morgan('tiny')); // logger
 app.use(cors());
 app.use(express.json());
+app.use(express.static('./public'));
+
+app.get('/url/:id', (req, res) => {
+	// TODO: get short url from id
+});
 
 app.get('/:id', (req, res) => {
 	// TODO: redirect to URL
